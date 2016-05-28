cfac2db9461516a57a29c787fcca9f4933343dde
drumguy16 <ripley.brett@gmail.com>
2016-05-28 12:49:48 -0400

    Add seed data for categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160528162344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",                                       null: false
+    t.string   "image_path",  default: "assets/default.png"
+    t.string   "description", default: ""
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.create(name: 'architecture', image_path: 'assets/architecture.jpg')
+Category.create(name: 'sculpture', image_path: 'assets/sculpture.jpg')
+Category.create(name: 'art', image_path: 'assets/art.jpeg')
+Category.create(name: 'about', image_path: 'assets/about.jpg')
+
+categories = Category.all
+
+
