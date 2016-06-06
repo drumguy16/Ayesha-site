3cf08acb6b1f14ee5d054af13448bdcca9e9b6eb
drumguy16 <ripley.brett@gmail.com>
2016-06-05 23:38:28 -0400

    Add hover effects to image links on category index.

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,5 +1,5 @@
 class Pic < ActiveRecord::Base
-	has_attached_file :pic_path, styles: { medium: "300x300>", thumb: "100x100>", large: "500x500" }, default_url: "/images/:style/missing.png", :default_style => :medium
+	has_attached_file :pic_path, styles: { medium: "300x300", thumb: "100x100>", large: "500x500" }, default_url: "/images/:style/missing.png", :default_style => :medium
 
 	validates_attachment :pic_path, 
       content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] },
